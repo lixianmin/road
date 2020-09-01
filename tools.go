dfb2d43ba487d920799a4470d5e11623420412b5
lixianmin <lixianmin@live.cn>
2020-09-01 17:12:28 +0800

    加入开放接口

---
+++ tools.go
@@ -0,0 +1,35 @@
+package bugfly
+
+import (
+	"context"
+	"github.com/lixianmin/bugfly/ifs"
+	"github.com/lixianmin/bugfly/logger"
+	"time"
+)
+
+/********************************************************************
+created:    2020-09-01
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+func GetSessionFromCtx(ctx context.Context) *Session {
+	fetus := ctx.Value(ifs.CtxKeySession)
+	if fetus == nil {
+		logger.Warn("ctx doesn't contain the session")
+		return nil
+	}
+
+	return fetus.(*Session)
+}
+
+func GetBeginTimeFromCtx(ctx context.Context) time.Time {
+	fetus := ctx.Value(ifs.CtxKeyBeginTime)
+	if fetus == nil {
+		logger.Warn("ctx doesn't contain beginTime")
+		return time.Time{}
+	}
+
+	return fetus.(time.Time)
+}
