9f69fc0013a1cc72b30e83ab294918989cea3132
lixianmin <lixianmin@live.cn>
2020-09-03 18:03:46 +0800

    加入client部分的代码

---
+++ session/handshake_data.go
@@ -0,0 +1,24 @@
+package session
+
+/********************************************************************
+created:    2020-09-03
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+// HandshakeClientData represents information about the client sent on the handshake.
+type HandshakeClientData struct {
+	Platform    string `json:"platform"`
+	LibVersion  string `json:"libVersion"`
+	BuildNumber string `json:"clientBuildNumber"`
+	Version     string `json:"clientVersion"`
+}
+
+// HandshakeData represents information about the handshake sent by the client.
+// `sys` corresponds to information independent from the app and `user` information
+// that depends on the app and is customized by the user.
+type HandshakeData struct {
+	Sys  HandshakeClientData    `json:"sys"`
+	User map[string]interface{} `json:"user,omitempty"`
+}
